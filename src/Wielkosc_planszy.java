3dffca199ff3c3cab3fa71e6b6cf1be887e5fa96
Vimmor <luczmateusz@gmail.com>
2019-06-10 01:51:30 +0200

    Implementation changing board size panel

---
+++ src/Wielkosc_planszy.java
@@ -0,0 +1,41 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Wielkosc_planszy extends JFrame implements ActionListener {
+    JTextField wielkosc;
+    JLabel w;
+    JButton zmien_wielkosc;
+
+    int rozmiar_planszy = 3;
+
+    public Wielkosc_planszy(){
+        super("Rozmiar planszy");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLocation(400, 200);
+        this.setSize(600, 90);
+        this.setLayout(null);
+
+        w = new JLabel("Podaj rozmiar planszy, na ktorej bedziesz gral:");
+        //Mozemy okreslic dokladnie polozenie i rozmiar komponentu, po uprzednim ustawieniu layoutu na null
+        w.setBounds(20, 20, 200, 20);
+        add(w);
+
+        wielkosc = new JTextField("");
+        wielkosc.setBounds(220, 20, 150, 20);
+        add(wielkosc);
+
+        zmien_wielkosc = new JButton("Zapisz wielkość");
+        zmien_wielkosc.setBounds(370, 20, 200, 20);
+        add(zmien_wielkosc);
+        zmien_wielkosc.addActionListener(this);
+
+        setVisible(false);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e){
+        rozmiar_planszy = Integer.valueOf(wielkosc.getText());
+        setVisible(false);
+    }
+}
